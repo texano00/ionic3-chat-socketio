1f770aa8385063721bac1bc368789c9cab7db54c
Yuri <yuribacciarini@gmail.com>
2018-04-26 17:13:23 +0200

    Update server address

--- src/app/chat-room/chat-room.module.ts
+++ src/app/chat-room/chat-room.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ChatRoomPage } from './chat-room';
 import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
-const config: SocketIoConfig = { url: 'http://172.20.4.129:3001', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
 
 @NgModule({
   declarations: [ChatRoomPage],
